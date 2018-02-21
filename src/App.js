a32a6ff42fdf3ba215e0609301fcb3f249428a53
BVCAD\pasante_innovacion <pasanteinnovacion@bvc.com.co>
2018-02-21 08:27:56 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-         HOLA MUNDO <code>src/App.js</code> and save to reload.
+        CHAO MUNDO <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
